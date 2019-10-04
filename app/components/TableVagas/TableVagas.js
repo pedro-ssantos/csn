41472ee487d85c4a8f01f37541b907aa805eae53
Pedro Santos <peusantos@gmail.com>
2019-10-04 18:11:49 -0300

    Tabela Vagas refatorada.

--- app/components/TableVagas/TableVagas.js
+++ app/components/TableVagas/TableVagas.js
@@ -25,30 +25,57 @@ const useStyles = makeStyles(theme => ({
 
 export default function TableVagas(props) {
   const classes = useStyles();
-  const [shift, setShift] = useState({
-    matutino: false,
-    vespertino: false,
-    noturno: false,
-    integral: false,
+  const [options, setOptions] = useState([
+    { name: 'matutino', value: 'matutino', status: false },
+    { name: 'vespetino', value: 'vespertino', status: false },
+    { name: 'noturno', value: 'noturno', status: false },
+    { name: 'integral', value: 'integral', status: false },
+  ]);
 
-    a: '5000',
-    b: '4000',
-    c: '3000',
-    d: '2000',
-    e: '1000',
-  });
-
-  useEffect(() => {
-    if (shift.checkedA) {
-      console.log('FAZFUNFAR');
-    }
-  });
+  const [vagas, setVagas] = useState([
+    {
+      turno: 'matutino',
+      vagasNovas: 0,
+      vagasRemanecentes: 0,
+      vagasProgramasEspeciais: 0,
+      inscritosVagasNovas: 0,
+      inscritosVagasRemanecentes: 0,
+      inscritosVagasProgramasEspeciais: 0,
+    },
+    {
+      turno: 'vespertino',
+      vagasNovas: 0,
+      vagasRemanecentes: 0,
+      vagasProgramasEspeciais: 0,
+      inscritosVagasNovas: 0,
+      inscritosVagasRemanecentes: 0,
+      inscritosVagasProgramasEspeciais: 0,
+    },
+    {
+      turno: 'noturno',
+      vagasNovas: 0,
+      vagasRemanecentes: 0,
+      vagasProgramasEspeciais: 0,
+      inscritosVagasNovas: 0,
+      inscritosVagasRemanecentes: 0,
+      inscritosVagasProgramasEspeciais: 0,
+    },
+    {
+      turno: 'integral',
+      vagasNovas: 0,
+      vagasRemanecentes: 0,
+      vagasProgramasEspeciais: 0,
+      inscritosVagasNovas: 0,
+      inscritosVagasRemanecentes: 0,
+      inscritosVagasProgramasEspeciais: 0,
+    },
+  ]);
 
   const updateField = e => {
     const value =
       e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-    setShift({
-      ...shift,
+    setOptions({
+      ...options,
       [e.target.name]: value,
     });
   };
@@ -70,325 +97,87 @@ export default function TableVagas(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <TableCell>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="matutino"
-                    value="SomeValue"
-                    onChange={updateField}
-                  />
-                }
-                label="Matutino"
-                style={{ width: 120 }}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.matutino}
-                name="a"
-                id="outlined-number"
-                value={shift.a}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.matutino}
-                name="b"
-                id="outlined-number"
-                value={shift.b}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.matutino}
-                id="outlined-number"
-                value="1000000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.matutino}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.matutino}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.matutino}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-          </TableRow>
-
-          <TableRow>
-            <TableCell>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="vespertino"
-                    value="SomeValue"
-                    onChange={updateField}
-                  />
-                }
-                label="Vespertino"
-                style={{ width: 120 }}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.vespertino}
-                name="a"
-                id="outlined-number"
-                value={shift.a}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.vespertino}
-                name="b"
-                id="outlined-number"
-                value={shift.b}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.vespertino}
-                id="outlined-number"
-                value="1000000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.vespertino}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.vespertino}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.vespertino}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-          </TableRow>
-
-          <TableRow>
-            <TableCell>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="noturno"
-                    value="SomeValue"
-                    onChange={updateField}
-                  />
-                }
-                label="Noturno"
-                style={{ width: 120 }}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.noturno}
-                name="a"
-                id="outlined-number"
-                value={shift.a}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.noturno}
-                name="b"
-                id="outlined-number"
-                value={shift.b}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.noturno}
-                id="outlined-number"
-                value="1000000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.noturno}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.noturno}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.noturno}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-          </TableRow>
-
-          <TableRow>
-            <TableCell>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    name="integral"
-                    value="SomeValue"
-                    onChange={updateField}
-                  />
-                }
-                label="Integral"
-                style={{ width: 120 }}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.integral}
-                name="a"
-                id="outlined-number"
-                value={shift.a}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.integral}
-                name="b"
-                id="outlined-number"
-                value={shift.b}
-                type="number"
-                margin="normal"
-                variant="outlined"
-                onChange={updateField}
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.integral}
-                id="outlined-number"
-                value="1000000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.integral}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.integral}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-            <TableCell align="right" size="small">
-              <TextField
-                disabled={!shift.integral}
-                id="outlined-number"
-                value="1000"
-                type="number"
-                margin="normal"
-                variant="outlined"
-              />
-            </TableCell>
-          </TableRow>
+          {options.map(option => (
+            <TableRow>
+              <TableCell>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name={option.name}
+                      value={option.value}
+                      onChange={updateField}
+                    />
+                  }
+                  label={option.name}
+                  style={{ width: 120 }}
+                />
+              </TableCell>
+              <TableCell align="right" size="small">
+                <TextField
+                  //disabled={!shift.matutino}
+                  name="a"
+                  id="outlined-number"
+                  //value={shift.a}
+                  type="number"
+                  margin="normal"
+                  variant="outlined"
+                  onChange={updateField}
+                />
+              </TableCell>
+              <TableCell align="right" size="small">
+                <TextField
+                  //disabled={!shift.matutino}
+                  name="b"
+                  id="outlined-number"
+                  //value={shift.b}
+                  type="number"
+                  margin="normal"
+                  variant="outlined"
+                  onChange={updateField}
+                />
+              </TableCell>
+              <TableCell align="right" size="small">
+                <TextField
+                  //disabled={!shift.matutino}
+                  id="outlined-number"
+                  value="1000000"
+                  type="number"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </TableCell>
+              <TableCell align="right" size="small">
+                <TextField
+                  //disabled={!shift.matutino}
+                  id="outlined-number"
+                  value="1000"
+                  type="number"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </TableCell>
+              <TableCell align="right" size="small">
+                <TextField
+                  //disabled={!shift.matutino}
+                  id="outlined-number"
+                  value="1000"
+                  type="number"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </TableCell>
+              <TableCell align="right" size="small">
+                <TextField
+                  //disabled={!shift.matutino}
+                  id="outlined-number"
+                  value="1000"
+                  type="number"
+                  margin="normal"
+                  variant="outlined"
+                />
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </TableMaterial>
     </Paper>
